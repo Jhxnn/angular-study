6d031649e3a7a6a367d872097a4f961f6541b9d8
Hilixx <jhonalizan@gmail.com>
2025-06-16 14:58:28 -0300

    lugares

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../lugar.service';
+import { Lugar } from '../lugar';
 
 @Component({
   selector: 'app-lugar',
   templateUrl: './lugar.component.html',
   styleUrl: './lugar.component.scss'
 })
-export class LugarComponent {
+export class LugarComponent implements OnInit{
 
 
   camposForm: FormGroup;
   categorias: Categoria[] = []
 
-  constructor(){
+  constructor(private categoriaService: CategoriaService, private service: LugarService){
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
@@ -23,7 +26,24 @@ export class LugarComponent {
     })
   }
 
+  ngOnInit(): void {
+      this.categoriaService.listarCategorias().subscribe({
+        next: (listaCategorias) =>{ this.categorias = listaCategorias
+          console.log(listaCategorias)
+        }
+      })
+  }
+
   salvar(){
-    console.log(this.camposForm.value)
+    this.service.salvar(this.camposForm.value).subscribe({
+      next: (lugar) => {console.log("Salvo com sucesso ", lugar)
+        this.camposForm.reset()
+      },
+      error: erro => console.log(erro)
+    })
+  }
+  isCampoInvalido(nomeCampo : string) :boolean{
+    const campo = this.camposForm.get(nomeCampo)
+    return campo?.invalid  && campo?.touched && campo?.errors?.['required']
   }
 }
